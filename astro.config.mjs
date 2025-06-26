ba8f37d2d7b47aabbef5addbf990e63d1bd466fd
Dan Fessler <fezzinate@gmail.com>
2025-06-26 11:49:40 -0700

    obfuscating email links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+const isDev = process.env.NODE_ENV !== "production";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dan.fessler.me",
+  site: isDev ? "http://localhost:4321" : "https://dan.fessler.me",
   integrations: [react()],
 });
