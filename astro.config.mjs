d359f0a86963a641c45f6c97ce2b17cad3713ae4
Dan Fessler <fezzinate@gmail.com>
2025-07-08 16:04:58 -0700

    adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 const isDev = process.env.NODE_ENV !== "production";
 
 // https://astro.build/config
 export default defineConfig({
   site: isDev ? "http://localhost:4321" : "https://danfessler.com",
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
 });
